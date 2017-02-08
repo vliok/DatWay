ccc2fae46841b59fbf7091e27f1106479cd08fc5
yawata159 <seijiyawata@gmail.com>
2017-02-07 22:30:37 -0500

    teacher stuff

--- teacher_db.py
+++ teacher_db.py
@@ -5,12 +5,17 @@ import pprint
 server = MongoClient("149.89.150.100")
 db = server.DatWay
 students_coll = db.students
+teachers_coll = db.teachers
 
-c = students_coll.find({})
-for doc in c:
-    pprint.pprint(doc)
+def get_ids(class_name):
+    ret = list()
+    results = students_coll.find({ class_name: {'$exists': True} })
+    for result in results:
+        del result['_id']
+        ret.append(result)
+    return ret
 
-with open('teachers.csv', 'r') as f:
+with open('teachers.csv') as f:
     reader = csv.DictReader(f)
-
-
+    for row in reader:
+        teachers_coll.insert_many( get_ids(row['code']) )
